88be3a54337bd4d12f65b6f82560dac085a67bc0
koit <koit3572@gmail.com>
2025-07-22 06:25:42 +0900

    2025-07-22-5

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ❗ Netlify에서 빌드 실패 방지
   },
-  output: "export", // 정적 사이트로 배포 시 필요
+  // output: "export", // 정적 사이트로 배포 시 필요
 };
 
 export default nextConfig;
--- src/app/work_book/[...slug]/page.tsx
+++ src/app/work_book/[...slug]/page.tsx
@@ -1,14 +1,36 @@
-import { getMarkdownBySlug } from "@/lib/markdown";
+// import { getAllSlugs, getHtmlFromMarkdown, getMarkdownBySlug } from "@/lib/markdown";
+
+// export async function generateStaticParams() {
+//   const slugs = getAllSlugs(); // [['hello'], ['a', 'test1']]
+//   return slugs.map((slug) => ({ slug }));
+// }
+
+// type Props = {
+//   params: {
+//     slug: string[];
+//   };
+// };
+
+// export default async function MarkdownPage({ params }: Props) {
+
+//     const html = await getHtmlFromMarkdown(params.slug);
+//     return (
+//       <div className="markdown-body mx-auto px-4 py-10">
+//         <h1 className="markdown-title">{html.data.title}</h1>
+//         <div dangerouslySetInnerHTML={{ __html: html.content }} />
+//       </div>
+//     );
+// }
+
+import { getAllSlugs, getMarkdownBySlug } from "@/lib/markdown";
 import ClientPage from "./ClientPage";
 
-interface PageProps {
-  params: {
-    slug: string[];
-  };
-}
-
-export default function MarkdownPage({ params }: PageProps) {
-  const { data, content } = getMarkdownBySlug(params.slug);
+export default async function MarkdownPage({
+  params,
+}: {
+  params: { slug: string[] };
+}) {
+  const { data, content } = await getMarkdownBySlug(params.slug);
 
   return (
     <div className="markdown-body mx-auto px-4 py-10">
@@ -16,4 +38,10 @@ export default function MarkdownPage({ params }: PageProps) {
       <ClientPage markdown={content} />
     </div>
   );
+}
+
+export async function generateStaticParams(): Promise<{ slug: string[] }[]> {
+  const allSlug = getAllSlugs();
+  const formatAllSlug = allSlug.map((data) => ({ slug: [...data] }));
+  return formatAllSlug;
 }
