8ef3a34437d13dbe08141d3e76975546dd29c2ea
koit <koit3572@gmail.com>
2025-07-22 06:07:21 +0900

    2025-07-22-2

--- src/app/work_book/[...slug]/page.tsx
+++ src/app/work_book/[...slug]/page.tsx
@@ -1,37 +1,13 @@
-
-// import { getAllSlugs, getHtmlFromMarkdown, getMarkdownBySlug } from "@/lib/markdown";
-
-// export async function generateStaticParams() {
-//   const slugs = getAllSlugs(); // [['hello'], ['a', 'test1']]
-//   return slugs.map((slug) => ({ slug }));
-// }
-
-// type Props = {
-//   params: {
-//     slug: string[];
-//   };
-// };
-
-// export default async function MarkdownPage({ params }: Props) {
-
-//     const html = await getHtmlFromMarkdown(params.slug);
-//     return (
-//       <div className="markdown-body mx-auto px-4 py-10">
-//         <h1 className="markdown-title">{html.data.title}</h1>
-//         <div dangerouslySetInnerHTML={{ __html: html.content }} />
-//       </div>
-//     );
-// }
-
-
-
-
-
 import { getMarkdownBySlug } from "@/lib/markdown";
 import ClientPage from "./ClientPage";
 
+interface PageProps {
+  params: {
+    slug: string[];
+  };
+}
 
-export default async function MarkdownPage({ params }: { params: { slug: string[] } }) {
+export default async function MarkdownPage({ params }: PageProps) {
   const { data, content } = await getMarkdownBySlug(params.slug);
 
   return (
@@ -41,9 +17,3 @@ export default async function MarkdownPage({ params }: { params: { slug: string[
     </div>
   );
 }
-
-
-
-
-
-
