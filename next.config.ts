ea701e914468942d43c94b48dde0c5e6a6cc0555
koit <koit3572@gmail.com>
2025-07-22 05:54:56 +0900

    2025-07-22-2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ❗ Netlify에서 빌드 실패 방지
+  },
+  output: "export", // 정적 사이트로 배포 시 필요
 };
 
 export default nextConfig;
